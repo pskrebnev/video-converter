d4443d3639ef1ddcfbe25861af0aa6d7ebf09446
Pavel Skrabneu <skrebnev@gmail.com>
2026-01-06 22:21:58 +0100

    ConverterApplication draft

--- src/main/kotlin/org/converter/ConverterApplication.kt
+++ src/main/kotlin/org/converter/ConverterApplication.kt
@@ -1,4 +1,12 @@
 package org.converter
 
-class ConverterApplication {
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConverterApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConverterApplication>(*args)
 }
+
