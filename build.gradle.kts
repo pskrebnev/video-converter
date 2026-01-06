bde08f70c991f04378a7dd8eeba5a313dc250522
Pavel Skrabneu <skrebnev@gmail.com>
2026-01-06 22:34:14 +0100

    build.gradle.kts changed: removed deprecated items

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -10,18 +11,33 @@ plugins {
 group = "org.videoconverter"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 kotlin {
     jvmToolchain(24)
 }
 
-tasks.test {
+tasks.withType<KotlinCompile> {
+    compilerOptions { // 2. Use compilerOptions instead of kotlinOptions
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
